76a4f5a5012e8ddc03c1646779c9ce911a2a6554
Tarun Tailor <tarun@crimecheck.in>
2023-10-10 22:42:36 +0530

    Creatde encryptKey.js and written script to encrypt .env keys and created encryptedKey.json in which our encryptd key is there now we can us ethat key instead of using .env varible

---
+++ ether-simple-storage/encryptKey.js
@@ -0,0 +1,20 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+    console.log(wallet, "wallet");
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD
+    )
+    console.log(encryptedJsonKey, 'encryptedJsonKey'); 
+    fs.writeFileSync("./encryptedKey.json", encryptedJsonKey);
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error, "Error");
+    process.exit(1);
+  });
+
